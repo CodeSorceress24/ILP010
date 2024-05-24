fe4b62f52abb80c1be62e6eae569c38ab66a2d27
CodeSorceress24 <marcela.maciel2408@gmail.com>
2024-05-23 23:52:07 -0300

    Continuação da lista de exercícios.

--- ProjetosJavaILP010/ExerciciosJava/src/Lote1p1/Ex12Lote1p1.java
+++ ProjetosJavaILP010/ExerciciosJava/src/Lote1p1/Ex12Lote1p1.java
@@ -8,5 +8,14 @@ import javax.swing.JOptionPane;
 public class Ex12Lote1p1 {
     public static void main(String[] args) {
         // declaração de variáveis.
+        int anoNascimento, anoAtual, idade, idadeFutura;
+        // entrada de dados.
+        anoNascimento = Integer.parseInt(JOptionPane.showInputDialog("Digite o ano de nascimento: "));
+        anoAtual = Integer.parseInt(JOptionPane.showInputDialog("Digite o ano atual: "));
+        // processamento de dados.
+        idade = anoAtual-anoNascimento;
+        idadeFutura = idade+17;
+        // saída de dados.
+        JOptionPane.showMessageDialog(null,"A idade atual é de: " + idade + " anos. E a idade futura será: " + idadeFutura);
     }
 }
